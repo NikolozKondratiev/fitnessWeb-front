ed1210af5646967e3ec250309cbc313f5d877cce
Nikoloz Kondratiev <nikolozkondratievi@gmail.com>
2023-06-07 20:09:13 +0400

    Modify tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["bpg", "serif"],
+        main2: ["Saira Condensed"],
+        main3: ["Staatliches", "cursive"],
+      },
+      height: {
+        halfScreen: "80vh",
+        smallerSection: "15vh",
+        section: "50vh",
+        largerSection: "70vh",
+        xlSection: "90vh",
+        bigPic: "55vh",
+        biggerPic: "70vh",
+      },
+      width: {
+        mid: "40rem",
+        xs: "30rem",
+      },
+      colors: {
+        white: "#FFF",
+        footer: "#212529",
+        footerSecndary: "#1C2023",
+      },
+    },
   },
   plugins: [],
 };
