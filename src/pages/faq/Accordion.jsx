789d67fe073089fabe580b4ee3ddcbbfed41bae0
Nikoloz Kondratiev <nikolozkondratievi@gmail.com>
2023-06-11 17:07:21 +0400

    Create FAQ component

---
+++ src/pages/faq/Accordion.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef } from "react";
+import QA from "../../QA.json";
+
+function Accordion() {
+  const [activeAccordion, setActiveAccordion] = useState(null);
+  const contentRefs = useRef([]);
+
+  const handleToggle = (id) => {
+    setActiveAccordion(id === activeAccordion ? null : id);
+  };
+
+  const getContentHeight = (id) => {
+    return contentRefs.current[id - 1].scrollHeight + "px";
+  };
+
+  return (
+    <div className="relative h-auto w-full">
+      {QA.map((qa) => (
+        <div
+          key={qa.id}
+          className="relative mb-1 flex w-full flex-col border border-gray-300 bg-gray-100"
+        >
+          <div
+            onClick={() => handleToggle(qa.id)}
+            className="flex h-11 cursor-pointer items-center justify-start px-5"
+          >
+            <h1 className="font-main2 text-base font-semibold text-black">
+              {qa.question}
+            </h1>
+          </div>
+          <div
+            className="overflow-hidden bg-gray-50 delay-100 duration-200 ease-in"
+            style={{
+              height:
+                qa.id === activeAccordion ? getContentHeight(qa.id) : "0px",
+            }}
+          >
+            <p
+              ref={(ref) => (contentRefs.current[qa.id - 1] = ref)}
+              className="px-5 pb-10 pt-5 font-main2 text-gray-500"
+            >
+              {qa.answer}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
---
+++ src/pages/faq/FAQ.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Accordion from "./Accordion";
+
+function FAQ() {
+  return (
+    <div className="relative my-5 flex h-auto justify-center overflow-hidden px-5 md:px-24 lg:px-48">
+      <div className="absolute -left-1/4 top-0 -z-20 h-xlSection w-xl rotate-45 scale-150 bg-gray-100"></div>
+      <div className="flex flex-col items-center gap-10 lg:w-1/2">
+        <h1 className="font-main2 text-3xl text-black">
+          Frequently Asked Questions
+        </h1>
+        <p className="text-center font-main2 text-base text-gray-500">
+          Please find detailed information below on our most commonly asked
+          questions. If you feel your question has still not been answered, then
+          just contact us below, and we will help you the best we can.
+        </p>
+
+        <Accordion />
+
+        <div className="flex flex-col items-center">
+          <p className="font-main2 text-2xl font-thin text-gray-800">
+            Still have some <strong>Questions?</strong>
+          </p>
+          <p className="font-main2 text-base text-gray-800">Let us help you</p>
+        </div>
+        <a className="cursor-pointer border-2 border-red-600 px-5 py-1 font-main2 font-thin text-gray-800 duration-200 ease-in hover:bg-red-600 hover:font-normal">
+          Send us an email...
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default FAQ;
